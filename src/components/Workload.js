4ba1fbe3e38002d57c57df2e011b30128ca61146
markusdreyer <markus.a.dreyer@gmail.com>
2020-11-02 15:37:21 +0100

    Merge changes

--- src/components/Workload.js
+++ src/components/Workload.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDataQuery } from "@dhis2/app-runtime"
 import {
+    NoticeBox,
     Table,
     TableCell,
     TableRow,
     TableBody,
-    Button
+    Button,
+    SwitchField
 } from '@dhis2/ui';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -20,7 +22,7 @@ const query = {
             orgUnit: "iVgNipWEgvE",
             ouMode: "SELECTED",
             programStatus: "ACTIVE",
-            status: "SCHEDULE",
+            status: "ACTIVE",
             page: 1,
             totalPages: true
         }
@@ -29,11 +31,11 @@ const query = {
         resource: "events",
         params: {
             fields: "*",
-            program: "uYjxkTbwRNf",
+            program: "DM9n1bUw8W8",
             orgUnit: "iVgNipWEgvE",
             ouMode: "SELECTED",
             programStatus: "ACTIVE",
-            status: "SCHEDULE",
+            status: "ACTIVE",
             page: 1,
             totalPages: true
         }
@@ -43,10 +45,15 @@ const query = {
 
 
 const Workload = () => {
+    const [tglSwitch, setTglSwitch] = useState({
+        indexcases: true,
+        contacts: false,
+    });
     const { loading, error, data } = useDataQuery(query)
     const [workload, setWorkload] = useState()
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(null);
+    const [typeError, setTypeError] = useState(false)
 
     const onChange = dates => {
         const [start, end] = dates;
@@ -54,38 +61,94 @@ const Workload = () => {
         setEndDate(end);
     };
 
+    const switchChange = (event) => {
+        setTglSwitch({ ...tglSwitch, [event.name]: event.checked })
+        //console.log(event);
+    }
 
     useEffect(() => {
-        console.log(data);
+        //console.log(data);
 
     }, [data])
 
     const calculateWorkload = () => {
         const startEpoch = startDate.getTime()
-        const endEpoch = endDate.getTime()
-        const tmpWorkload = {
-            indexCases: 0,
-            contactCases: 0,
-            total: 0
-        }
-        data.workload.events.map(event => {
-            const dueDate = Date.parse(event.dueDate)
-            if (dueDate >= startEpoch && dueDate <= endEpoch) {
-                if (event.program == "uYjxkTbwRNf") {
-                    tmpWorkload.indexCases++
-                } else if (event.program == "DM9n1bUw8W8") {
-                    tmpWorkload.contactCases++
-                } else {
-                    console.log("Program not recognized")
-                }
-                tmpWorkload.total++
+
+        //Sjekker at endDate ikke er null
+        try {
+            const endEpoch = endDate.getTime()
+
+            const merged = data.indexCases.events.concat(data.contactCases.events)
+
+            const tmpWorkload = {
+                indexCases: 0,
+                contactCases: 0,
+                total: 0
             }
-        })
-        setWorkload(tmpWorkload)
+
+            merged.map(event => {
+
+                const dueDate = Date.parse(event.dueDate)
+
+                if (dueDate >= startEpoch && dueDate <= endEpoch) {
+                    if (event.program == "uYjxkTbwRNf") {
+                        tmpWorkload.indexCases++
+                    } else if (event.program == "DM9n1bUw8W8") {
+                        tmpWorkload.contactCases++
+                    } else {
+                        console.log("Program not recognized")
+                    }
+                    tmpWorkload.total++
+                }
+            })
+            setWorkload(tmpWorkload), setTypeError(false)
+        }
+        catch (e) {
+            //Treng berre ein av desse kanskje?
+            if (e instanceof TypeError && endDate === null) {
+                setTypeError(true);
+                //bedre med metode enn state?
+            }
+
+        }
     }
 
     return (
         <div id="workload-content">
+            <h1>Workload</h1>
+            <section>
+                <>
+                    <SwitchField
+                        checked={tglSwitch.indexcases}
+                        dataTest="dhis2-uiwidgets-switchfield"
+                        label="Index cases"
+                        name="indexcases"
+                        onChange={switchChange}
+                        value="checked"
+                    />
+                    <SwitchField
+                        checked={tglSwitch.contacts}
+                        dataTest="dhis2-uiwidgets-switchfield"
+                        label="Contacts"
+                        name="contacts"
+                        onChange={switchChange}
+                        value="unchecked"
+                    />
+                </>
+            </section>
+
+            {!typeError &&
+                <p>Velg startdato og sluttdato for når du ønsker å vite arbeidsmengden</p>
+            }
+            {typeError &&
+                <NoticeBox
+                    dataTest="dhis2-uicore-noticebox"
+                    title="Velg en sluttdato"
+                    warning
+                >
+                    Husk å velg en sluttdato. Nå er det bare valgt en startdato.
+            </NoticeBox>
+            }
             <div className="date-picker">
                 <DatePicker
                     selected={startDate}
@@ -94,44 +157,59 @@ const Workload = () => {
                     endDate={endDate}
                     selectsRange
                     inline
+
                 />
             </div>
+            <Button
+                dataTest="dhis2-uicore-button"
+                name="Primary button"
+                primary
+                type="button"
+                value="default"
+                onClick={calculateWorkload}
+            >
+                Submit
+                            </Button>
 
-            <Table suppressZebraStriping className="workload-table">
-                <TableBody>
-                    <TableRow>
-                        <TableCell className="left-column">
-                            Index Cases
-                        </TableCell>
-                        <TableCell className="right-column">
-                            XXX
-                        </TableCell>
+            {workload && (
 
-                    </TableRow>
-                    <TableRow>
-                        <TableCell className="left-column">
-                            Contacts
+                <Table suppressZebraStriping className="workload-table">
+                    <TableBody>
+                        {tglSwitch.indexcases &&
+                            <TableRow>
+                                <TableCell className="left-column">
+                                    Index Cases
                         </TableCell>
-                        <TableCell className="right-column">
-                            XXX
+                                <TableCell className="right-column">
+                                    {workload.indexCases}
+                                </TableCell>
+                            </TableRow>
+                        }
+                        {tglSwitch.contacts &&
+                            <TableRow>
+                                <TableCell className="left-column">
+                                    Contacts
                         </TableCell>
-                            <TableCell dataTest="details-first-name">
-                                {workload.indexCases}
-                            </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell className="left-column">
-                            Total
+                                <TableCell className="right-column">
+                                    {workload.contactCases}
+                                </TableCell>
+
+                            </TableRow>
+                        }
+                        {(tglSwitch.indexcases && tglSwitch.contacts) &&
+                            <TableRow>
+                                <TableCell className="left-column">
+                                    Total
                         </TableCell>
-                        <TableCell className="right-column">
-                            XXX
-                        </TableCell>
-                            <TableCell dataTest="details-first-name">
-                                {workload.total}
-                            </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+                                <TableCell className="right-column">
+                                    {workload.total}
+                                </TableCell>
+
+                            </TableRow>
+                        }
+                    </TableBody>
+                </Table >
+            )}
         </div>
     );
 }
